f19226abf767b91650387a2051b176591bb07d5b
황규현 <57509844+hgyuhyeon@users.noreply.github.com>
2021-04-03 14:30:33 +0900

    Create 4999.cpp

---
+++ 4999_아!/4999.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+
+	string s1, s2;
+	cin >> s1 >> s2;
+
+	if (s1.length() >= s2.length())
+		cout << "go";
+	else
+		cout << "no";
+
+	return 0;
+}
