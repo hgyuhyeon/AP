74ae1ced2ab2806746c8d3ac995536bc6c5d25b0
hgyuhyeon <hwgyuhyeon@gmail.com>
2023-07-20 12:59:26 +0900

    leetcode 735 - daily

--- Leetcode/735_Asteroid_Collision.cpp
+++ Leetcode/735_Asteroid_Collision.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> s, s2;
+        stack<int> s;
         for (auto a: asteroids) {
             if (s.empty()) s.push(a);
             else {
-                if ((s.top() > 0 && a > 0) || s.top() < 0 && a < 0) s.push(a);
-                else {
+                if ((a > 0 && s.top() > 0) || (a < 0 && s.top() < 0)) {
+                    s.push(a);
+                }
+                else { // collide
                     while (!s.empty() && s.top() > 0 && s.top() < abs(a)) s.pop();
-                    if (!s.empty() && s.top() > abs(a)) continue;
-                    else if (!s.empty() && s.top() == abs(a)) s.pop();
+                    if (!s.empty() && (s.top() == abs(a))) s.pop();
+                    else if (!s.empty() && (s.top() > abs(a))) continue;
                     else s.push(a);
                 }
             }
         }
-        
-        vector<int> res(s.size());
-        for (int i = res.size()-1; i >= 0; i--) {
-            res[i] = s.top();
+
+        vector<int> ans(s.size());
+        for (int i = ans.size() - 1; i >= 0; i--) {
+            ans[i] = s.top();
             s.pop();
         }
-        return res;
+        return ans;
     }
 };
