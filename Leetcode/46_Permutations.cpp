2b395f7d7d3fb2d453106666be8280f7e00a19b5
hgyuhyeon <hwgyuhyeon@gmail.com>
2023-08-02 12:50:05 +0900

    leetcode 46 - update

--- Leetcode/46_Permutations.cpp
+++ Leetcode/46_Permutations.cpp
@@ -1,28 +1,11 @@
 class Solution {
 public:
-    vector<vector<int>> res;
-    vector<int> tmp;
-    map<int, bool> visited;
-    void permutation(vector<int>& nums) {
-        if (tmp.size() >= nums.size()) {
-            res.push_back(tmp);
-        }
-        else {
-            for (auto n: nums) {
-                if (!visited[n]) {
-                    tmp.push_back(n);
-                    visited[n] = true;
-                    permutation(nums);
-                    tmp.pop_back();
-                    visited[n] = false;
-                }
-            }
-        }
-    }
-    
     vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
-        permutation(nums);
-        return res;
+        do {
+            ans.push_back(nums);
+        } while (next_permutation(nums.begin(), nums.end()));
+        return ans;
     }
 };
