813d16b2feff85eb1aec66faa5a1a5e9b715731b
hgyuhyeon <hwgyuhyeon@gmail.com>
2023-08-03 14:23:00 +0900

    leetcode 17 - daily

--- Leetcode/17_Letter_Combinations_of_a_Phone_Number.cpp
+++ Leetcode/17_Letter_Combinations_of_a_Phone_Number.cpp
@@ -1,38 +1,18 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        map<int, string> m;
-        m[2] = "abc";
-        m[3] = "def";
-        m[4] = "ghi";
-        m[5] = "jkl";
-        m[6] = "mno";
-        m[7] = "pqrs";
-        m[8] = "tuv";
-        m[9] = "wxyz";
+        if (digits.size() == 0) return vector<string>();
+        vector<string> v = {
+            "", "", "abc", "def", "ghi", "jkl",
+            "mno", "pqrs", "tuv", "wxyz"
+        }, ans{ "" };
 
-        queue<string> q;
         for (auto n: digits) {
-            int num = (int)n - '0';
-            if (q.empty()) {
-                for (auto c: m[num]) {
-                    string str = string(1, c);
-                    q.push(str);
-                }
+            vector<string> tmp;
+            for (auto c: v[n - '0']) {
+                for (auto s: ans) tmp.push_back(s + c);
             }
-            else {
-                int len = q.front().size();
-                while (q.front().size() == len) {
-                    for (auto c: m[num]) q.push(q.front() + c);
-                    q.pop();
-                }
-            }
-        }
-
-        vector<string> ans;
-        while (!q.empty()) {
-            ans.push_back(q.front());
-            q.pop();
+            ans = tmp;
         }
 
         return ans;
