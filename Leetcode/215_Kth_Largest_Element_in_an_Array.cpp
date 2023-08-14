61e4e5a2ddc700a0b4f41db1f8ce1f7da18350d7
hgyuhyeon <hwgyuhyeon@gmail.com>
2023-08-14 11:09:10 +0900

    leetcode 215 - daily

---
+++ Leetcode/215_Kth_Largest_Element_in_an_Array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+        for (auto i: nums) pq.push(i);
+        int l = 1;
+        while (l++ < k) pq.pop();
+        return pq.top();
+    }
+};
