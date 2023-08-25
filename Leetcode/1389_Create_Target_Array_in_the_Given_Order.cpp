7e6c28ae9ba0f2f49048af79b91ee6c1daf0fb75
hgyuhyeon <hwgyuhyeon@gmail.com>
2023-08-25 13:54:41 +0900

    leetcode 1389 - update

--- Leetcode/1389_Create_Target_Array_in_the_Given_Order.cpp
+++ Leetcode/1389_Create_Target_Array_in_the_Given_Order.cpp
@@ -1,17 +1,9 @@
 class Solution {
 public:
     vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
-        vector<int> ans(nums.size(), -1);
-        
-        for (int i = 0; i < nums.size(); i++) {
-            if (ans[index[i]] == -1) ans[index[i]] = nums[i];
-            else {
-                for (int j = nums.size() - 1; j >= index[i] + 1; j--)
-                    ans[j] = ans[j-1];
-                ans[index[i]] = nums[i];
-            }
-        }
-
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); i++)
+            ans.insert(ans.begin() + index[i], nums[i]);
         return ans;
     }
 };
