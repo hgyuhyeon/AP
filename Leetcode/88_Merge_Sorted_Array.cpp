0587e1c606b08562e12969fd27a869fd24e0784b
hgyuhyeon <hwgyuhyeon@gmail.com>
2023-11-29 22:37:36 +0900

    leetcode 88

--- Leetcode/88_Merge_Sorted_Array.cpp
+++ Leetcode/88_Merge_Sorted_Array.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for (int i = m, j = 0; i < nums1.size(); i++, j++) nums1[i] = nums2[j];
-        sort(nums1.begin(), nums1.end());
+        int p = m-- + n-- - 1;
+        while (m >= 0 && n >= 0) nums1[p--] = (nums1[m] >= nums2[n]) ? nums1[m--] : nums2[n--];
+        while (n >= 0) nums1[p--] = nums2[n--];
     }
 };
