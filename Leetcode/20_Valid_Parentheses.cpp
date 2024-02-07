9fcfbc0a07eb8251566c1096d8db3f4c2f336406
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-02-07 14:55:45 +0900

    leetcode 20 - study

--- Leetcode/20_Valid_Parentheses.cpp
+++ Leetcode/20_Valid_Parentheses.cpp
@@ -3,29 +3,10 @@ public:
     bool isValid(string s) {
         stack<char> stk;
         for (auto c: s) {
-            switch(c) {
-                case '(':
-                case '{':
-                case '[':
-                    stk.push(c);
-                    break;
-                case ')':
-                    if (!stk.empty() && stk.top() == '(') stk.pop();
-                    else return false;
-                    break;
-                case '}':
-                    if (!stk.empty() && stk.top() == '{') stk.pop();
-                    else return false;
-                    break;
-                case ']':
-                    if (!stk.empty() && stk.top() == '[') stk.pop();
-                    else return false;
-                    break;
-                default:
-                    return false;
-            }
+            if (c == '(' || c == '{' || c == '[') stk.push(c);
+            else if (!stk.empty() && (c == ')' && (stk.top() == '(') || (c == '}' && stk.top() == '{') || (c == ']' && stk.top() == '['))) stk.pop();
+            else return false;
         }
-        if (!stk.empty()) return false;
-        else return true;
+        return stk.empty();
     }
 };
