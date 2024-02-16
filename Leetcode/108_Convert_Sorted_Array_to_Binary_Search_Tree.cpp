045ed0e1ad9c2362e674837cf5b70c35dcf2d50c
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-02-16 11:11:48 +0900

    leetcode 108 - study

--- Leetcode/108_Convert_Sorted_Array_to_Binary_Search_Tree.cpp
+++ Leetcode/108_Convert_Sorted_Array_to_Binary_Search_Tree.cpp
@@ -11,15 +11,15 @@
  */
 class Solution {
 public:
-    TreeNode* sortedArrayToBST(vector<int>& nums, int first, int last) {
-        if (first > last) return NULL;
-        int mid = (first + last) / 2;
+    TreeNode* BST(vector<int>& nums, int start, int end) {
+        if (start > end) return NULL;
+        int mid = (start + end) / 2;
         TreeNode* node = new TreeNode(nums[mid]);
-        node->left = sortedArrayToBST(nums, first, mid - 1);
-        node->right = sortedArrayToBST(nums, mid + 1, last);
+        node->left = BST(nums, start, mid - 1);
+        node->right = BST(nums, mid + 1, end);
         return node;
     }
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        return sortedArrayToBST(nums, 0, nums.size()-1);
+        return BST(nums, 0, nums.size() - 1);
     }
 };
