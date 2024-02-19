9a49614d7ed0007679b66721aeffa75ba55f8e7b
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-02-19 16:06:29 +0900

    leetcode 145 - study

---
+++ Leetcode/145_Binary_Tree_Postorder_Traversal.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> ans;
+    void dfs(TreeNode* node) {
+        if (!node) return;
+        dfs(node->left);
+        dfs(node->right);
+        ans.push_back(node->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+};
