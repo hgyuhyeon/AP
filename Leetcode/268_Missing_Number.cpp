29d99b58968b6c1ff1d1f010e9bc1bd0a6d5e0cb
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-02-20 20:30:38 +0900

    leetcode 268 - daily

--- Leetcode/268_Missing_Number.cpp
+++ Leetcode/268_Missing_Number.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        for (int i = 0; i < n; i++) if (i != nums[i]) return i;
-        return n;
+        int n = nums.size(), sum = (n * (n + 1)) / 2;
+        return sum - accumulate(nums.begin(), nums.end(), 0);
     }
 };
