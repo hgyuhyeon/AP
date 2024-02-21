47a20eca94a5ec606ad92a5b5c56270b09404d1e
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-02-21 15:09:44 +0900

    leetcode 380 - study

--- Leetcode/380_Insert_Delete_GetRandom_O(1).cpp
+++ Leetcode/380_Insert_Delete_GetRandom_O(1).cpp
@@ -1,24 +1,22 @@
 class RandomizedSet {
 public:
-    set<int> s;
-    
+    unordered_set<int> s;
     RandomizedSet() {
         s.clear();
     }
     
     bool insert(int val) {
-        auto ret = s.insert(val);
-        return ret.second;
+        return s.insert(val).second;
     }
     
     bool remove(int val) {
-        if (s.find(val) == s.end()) return false;
-        s.erase(val);
-        return true;
+        return s.erase(val);
     }
     
     int getRandom() {
-        return *next(s.begin(), rand() % s.size());
+        int num = rand() % s.size(), cnt = 0;
+        for (auto itr = s.begin(); itr != s.end(); itr++) if (cnt++ == num) return *itr;
+        return *s.begin();
     }
 };
 
