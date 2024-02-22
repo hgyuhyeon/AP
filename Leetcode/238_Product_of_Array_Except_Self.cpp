14b36f663a51598b9579c50b9ec88e7b971a180c
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-02-22 18:14:10 +0900

    leetcode 238 - study

---
+++ Leetcode/238_Product_of_Array_Except_Self.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, 1);
+        for (int i = 1; i < n; i++) ans[i] *= ans[i-1] * nums[i-1];
+        for (int i = n - 2, s = nums[n-1]; i >= 0; i--) {
+            ans[i] *= s;
+            s *= nums[i];
+        }
+        return ans;
+    }
+};
