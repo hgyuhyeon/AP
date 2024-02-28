6e266d52b934af3391e077871f8b0a39a9b83880
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-02-28 19:41:57 +0900

    leetcode 15 - study

--- Leetcode/15_3Sum.cpp
+++ Leetcode/15_3Sum.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
-        set<vector<int>> s;
         sort(nums.begin(), nums.end());
-        int one = 0, two = 1, three = 2;
-
-        while (one < nums.size() - 2) {
-            two = one + 1; three = nums.size() - 1;
-            while (two < three) {
-                if (nums[one] + nums[two] + nums[three] == 0) {
-                    s.insert({nums[one], nums[two], nums[three]});
-                    two++;
-                    three--;
-                }
-                else if (nums[one] + nums[two] + nums[three] < 0) {
-                    two++;
-                }
-                else three--;
+        set<vector<int>> s;
+        int n = nums.size(), idx = 1;
+        for (auto num: nums) {
+            int lo = idx++, hi = n-1, mid;
+            while (lo < hi) {
+                if (num + nums[lo] + nums[hi] == 0) s.insert({num, nums[lo++], nums[hi--]});
+                else if (num + nums[lo] + nums[hi] < 0) lo++;
+                else hi--;
             }
-            one++;
         }
-
         vector<vector<int>> ans(s.begin(), s.end());
         return ans;
     }
