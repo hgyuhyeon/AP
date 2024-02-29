b8be34123a28f8a3d6ce8034deed51c04ad8d0ac
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-02-29 17:47:27 +0900

    leetcode 50 - study

--- Leetcode/50_Pow(x,_n).cpp
+++ Leetcode/50_Pow(x,_n).cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
+    double solve(double x, int n) {
+        if (n == 1 || n == -1) return x;
+        else if (n == 0) return 1;
+        double ans = solve(x, n / 2);
+        return ans * ans * ((n%2) == 0 ? 1 : x);
+    }
     double myPow(double x, int n) {
-        double ans = 1;
-        if (n == 0) return ans;
-        if (x == 0) return 0;
-        else if (x == 1 || (x == -1 && n % 2 == 0)) return 1;
-        else if (x == -1) return -1; 
-
         if (n < 0) x = 1 / x;
-
-        if (n == INT_MIN) n++;
-        for (int i = 0; i < abs(n); i++) {
-            ans *= x;
-            if (ans > 10000 || ans == 0) break;
-        }
-        return ans;
+        return solve(x, n);
     }
 };
