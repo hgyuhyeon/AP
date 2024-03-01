5c0e82076aa6914feca6e4d77742c147c48e3048
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-03-01 11:48:03 +0900

    leetcode 11 - study

---
+++ Leetcode/11_Container_With_Most_Water.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int lo = 0, hi = height.size() - 1, ans = 0;
+        while (lo < hi) {
+            ans = max(ans, min(height[lo], height[hi]) * (hi - lo));
+            if (height[lo] < height[hi]) lo++;
+            else hi--;
+        }
+        return ans;
+    }
+};
