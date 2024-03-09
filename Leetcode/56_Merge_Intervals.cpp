8d8800c2b8df6bfd1c8e30834fcb5e6349f9f697
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-03-09 12:53:04 +0900

    leetcode 56 - study

--- Leetcode/56_Merge_Intervals.cpp
+++ Leetcode/56_Merge_Intervals.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> ans;        
-        if (intervals.size() == 1) return intervals;
-        
+        vector<vector<int>> ans;
+        int n = intervals.size();
         sort(intervals.begin(), intervals.end());
-        ans.push_back(intervals[0]);
-        
-        for (int i = 1; i < intervals.size(); i++) {
-            if (ans[ans.size()-1][1] >= intervals[i][0]) ans[ans.size()-1][1] = max(intervals[i][1], ans[ans.size()-1][1]);
-            else ans.push_back(intervals[i]);
+        vector<int> tmp = intervals[0];
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] > tmp[1]) {
+                ans.push_back(tmp);
+                tmp = intervals[i];
+            }
+            else tmp[1] = max(tmp[1], intervals[i][1]);
         }
-        
+        ans.push_back(tmp);
         return ans;
     }
 };
