a6b0f72711c1d2e7d9d602de8a00c536e7802fd5
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-03-10 11:15:20 +0900

    leetcode 57 - study

--- Leetcode/57_Insert_Interval.cpp
+++ Leetcode/57_Insert_Interval.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> ans;
-        int i = 0;
-        while (i < intervals.size() && intervals[i][1] < newInterval[0]) {
-            ans.push_back(intervals[i++]);
+        intervals.push_back(newInterval);
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end());
+        vector<int> tmp = intervals[0];
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] > tmp[1]) {
+                ans.push_back(tmp);
+                tmp = intervals[i];
+            }
+            else tmp[1] = max(tmp[1], intervals[i][1]);
         }
-        
-        if (i < intervals.size()) newInterval[0] = min(newInterval[0], intervals[i][0]);
-        while (i < intervals.size() && intervals[i][0] <= newInterval[1]) {
-            newInterval[1] = max(newInterval[1], intervals[i++][1]);
-        }
-        ans.push_back(newInterval);
-        
-        while (i < intervals.size()) {
-            ans.push_back(intervals[i++]);
-        }
-        
+        ans.push_back(tmp);
         return ans;
     }
 };
