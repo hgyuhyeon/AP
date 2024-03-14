415403ce98762c921a5c5816645429e3ad912df6
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-03-14 12:36:52 +0900

    leetcode 54 - study

--- Leetcode/54_Spiral_Matrix.cpp
+++ Leetcode/54_Spiral_Matrix.cpp
@@ -4,31 +4,18 @@ public:
         vector<int> ans;
         int n = matrix.size() - 1, m = matrix[0].size() - 1, ln = 0, lm = 0;
         while (n >= ln && m >= lm) {
-            for (int i = lm; i <= m; i++) {
-                if (matrix[ln][i] <= 100) {
-                    ans.push_back(matrix[ln][i]);
-                    matrix[ln][i] = 101;
-                }
+            for (int i = lm; i <= m; i++) ans.push_back(matrix[ln][i]);
+            ln++;
+            for (int i = ln; i <= n; i++) ans.push_back(matrix[i][m]);
+            m--;
+            if (n >= ln) {
+                for (int i = m; i >= lm; i--) ans.push_back(matrix[n][i]);
+                n--;
             }
-            for (int i = ln; i <= n; i++) {
-                if (matrix[i][m] <= 100) {
-                    ans.push_back(matrix[i][m]);
-                    matrix[i][m] = 101;
-                }
+            if (m >= lm) {
+                for (int i = n; i >= ln; i--) ans.push_back(matrix[i][lm]);
+                lm++;
             }
-            for (int i = m; i >= lm; i--) {
-                if (matrix[n][i] <= 100) {
-                    ans.push_back(matrix[n][i]);
-                    matrix[n][i] = 101;
-                }
-            }
-            for (int i = n; i >= ln; i--) {
-                if (matrix[i][lm] <= 100) {
-                    ans.push_back(matrix[i][lm]);
-                    matrix[i][lm] = 101;
-                }
-            }
-            lm++; ln++; m--; n--;
         }
         return ans;
     }
