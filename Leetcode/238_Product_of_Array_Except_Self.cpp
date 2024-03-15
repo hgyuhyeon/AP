3ef73aeee5abb25875d5721da41ee2cc2510ac64
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-03-15 12:20:03 +0900

    leetcode 238 - daily

--- Leetcode/238_Product_of_Array_Except_Self.cpp
+++ Leetcode/238_Product_of_Array_Except_Self.cpp
@@ -4,10 +4,7 @@ public:
         int n = nums.size();
         vector<int> ans(n, 1);
         for (int i = 1; i < n; i++) ans[i] *= ans[i-1] * nums[i-1];
-        for (int i = n - 2, s = nums[n-1]; i >= 0; i--) {
-            ans[i] *= s;
-            s *= nums[i];
-        }
+        for (int i = n - 2, s = nums[n-1]; i >= 0; s *= nums[i--]) ans[i] *= s;
         return ans;
     }
 };
