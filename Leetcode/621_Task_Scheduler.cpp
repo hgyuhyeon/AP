372faa3c2db31d9a9799634680a140e2097b218b
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-03-29 19:50:04 +0900

    leetcode 621 - daily

--- Leetcode/621_Task_Scheduler.cpp
+++ Leetcode/621_Task_Scheduler.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
         if (n == 0) return tasks.size();
-        vector<int> v(26,0);
+        vector<int> v(26, 0);
         int maxnum = 0;
-        
-        for (auto c: tasks) {
-            v[c - 'A']++;
-            maxnum = max(maxnum, v[c - 'A']);
-        }
-            
+
+        for (auto c: tasks) maxnum = max(maxnum, ++v[c - 'A']);
+
         int ans = (maxnum - 1) * (n + 1);
         for (auto i: v) if(i == maxnum) ans++;
-        
+
         return max((int)tasks.size(), ans);
     }
 };
