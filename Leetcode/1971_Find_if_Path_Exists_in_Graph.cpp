9be4563a11e0a5669e4d436b51a49eb7ffd2f9a3
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-04-21 11:38:24 +0900

    leetcode 1971 - daily

--- Leetcode/1971_Find_if_Path_Exists_in_Graph.cpp
+++ Leetcode/1971_Find_if_Path_Exists_in_Graph.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        if (source == destination) return true;
-
-        unordered_map<int, vector<int>> m;
-        for (auto i: edges) {
-            m[i[0]].push_back(i[1]);
-            m[i[1]].push_back(i[0]);
+        vector<vector<int>> g(n, vector<int>());
+        for (auto e: edges) {
+            g[e[0]].push_back(e[1]);
+            g[e[1]].push_back(e[0]);
         }
-
+        vector<bool> v(n, false);
         queue<int> q;
         q.push(source);
-
-        vector<bool> visited(n, false);
-        visited[source] = true;
-
         while (!q.empty()) {
-            int p = q.front();
+            int cur = q.front();
+            if (!v[cur]) {
+                v[cur] = true;
+                if (cur == destination) return true;
+                for (auto i: g[cur]) q.push(i);
+            }
             q.pop();
-            visited[p] = true;
-            if (p == destination) return true;
-            for (auto i: m[p]) if (!visited[i]) q.push(i);
         }
-        
         return false;
     }
 };
