5b070c9c8b8279ab2cc0ea3e6e1708bf2fca2125
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-04-21 19:52:53 +0900

    leetcode 207 - study

--- Leetcode/207_Course_Schedule.cpp
+++ Leetcode/207_Course_Schedule.cpp
@@ -2,26 +2,18 @@ class Solution {
 public:
     bool dfs(vector<vector<int>>& g, vector<int>& v, int i) {
         v[i] = 1;
-        for (auto cur: g[i]) {
-            if ((v[cur] == 0 && !dfs(g, v, cur)) || v[cur] == 1) return false;
-        }
+        for (auto cur: g[i]) if (v[cur] == 1 || (v[cur] == 0 && !dfs(g, v, cur))) return false;
         v[i] = 2;
         return true;
     }
-
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> g(numCourses);
-        vector<int> visited(numCourses, 0);
-
-        for (auto p: prerequisites) {
-            g[p[0]].push_back(p[1]);
-        }
-
+        vector<vector<int>> g(numCourses, vector<int>());
+        vector<int> v(numCourses, 0);
+        for (auto p: prerequisites) g[p[1]].push_back(p[0]);
         for (int i = 0; i < numCourses; i++) {
-            if (visited[i] == 2) continue;
-            else if (!dfs(g, visited, i)) return false;
+            if (v[i] == 2) continue;
+            if (!dfs(g, v, i)) return false;
         }
-
         return true;
     }
 };
