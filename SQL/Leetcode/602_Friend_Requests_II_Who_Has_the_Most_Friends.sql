05bd7592169c093703bc9b8b63d9e66fae569965
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-04-25 12:14:25 +0900

    leetcode 602 - study

---
+++ SQL/Leetcode/602_Friend_Requests_II_Who_Has_the_Most_Friends.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+with cte as(
+    select requester_id id from RequestAccepted
+    union all
+    select accepter_id id from RequestAccepted
+)
+
+select id, count(*) as num
+from cte
+group by id
+order by num desc 
+limit 1
