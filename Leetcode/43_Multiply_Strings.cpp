60f55f27b30730bc8e250898d4fa1ad432da3b36
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-05-03 20:27:51 +0900

    leetcode 43 - study

--- Leetcode/43_Multiply_Strings.cpp
+++ Leetcode/43_Multiply_Strings.cpp
@@ -2,41 +2,23 @@ class Solution {
 public:
     string multiply(string num1, string num2) {
         if (num1 == "0" || num2 == "0") return "0";
-        
-        vector<int> res(num1.size()+num2.size(), 0);
-        int index, temp;
-        
-        for (int i = num2.size() - 1, m = 1; i>=0; i--, m++) {
-            index = res.size() - m;
-            for (int j = num1.size() - 1; j >= 0; j--) {
-                temp = (num2[i] - '0') * (num1[j] - '0');
-                if (temp >= 10) res[index - 1] += temp / 10;
-                res[index] += temp % 10;
-                index--;
+        vector<int> mul(num1.size()+num2.size(), 0);
+        for (int i = mul.size()-1, j = num2.size()-1; j >= 0; i--, j--) {
+            int idx = i;
+            for (int k = num1.size()-1; k >= 0; k--) {
+                mul[idx] += ((num1[k] - '0') * (num2[j] - '0'));
+                if (mul[idx] / 10 > 0) {
+                    mul[idx-1] += (mul[idx]/10);
+                    mul[idx] %= 10;
+                }
+                idx--;
             }
         }
-        
-        temp = 0;
-        for (int i = res.size() - 1; i >= 0; i--) {
-            if (temp) {
-                res[i] += temp;    
-            }
-            
-            if (res[i] >= 10) {
-                temp = res[i] / 10;
-                res[i] %= 10;
-            }
-            else temp = 0;
-        }
-        
-        string ans = "";
+
         int i = 0;
-        while (!res[i]) i++;
-        
-        while (i < res.size()) {
-            ans += (res[i++] + '0');
-        }
-        
+        string ans = "";
+        while (mul[i] == 0) i++;
+        while (i < mul.size()) ans += to_string(mul[i++]);
         return ans;
     }
 };
