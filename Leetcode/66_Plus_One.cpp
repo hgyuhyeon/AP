35e83c48a1c72e14a8c91a1b69adfdae838f5d93
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-05-03 18:37:54 +0900

    leetcode 66 - study

--- Leetcode/66_Plus_One.cpp
+++ Leetcode/66_Plus_One.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        if (digits.back() != 9) {
-            digits.back() += 1;
-            return digits;
-        }
-        
         for (int i = digits.size() - 1; i >= 0; i--) {
             if (digits[i] != 9) {
                 digits[i] += 1;
