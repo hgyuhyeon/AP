6f757f1c3f949bb42b7aeac6794a277f515626f0
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-05-04 12:50:30 +0900

    leetcode 881 - daily

--- Leetcode/881_Boats_to_Save_People.cpp
+++ Leetcode/881_Boats_to_Save_People.cpp
@@ -2,11 +2,19 @@ class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
         sort(people.begin(), people.end());
-
-        int lo = 0, hi = people.size()-1, ans = 0;
-        while (lo <= hi) {
-            if (people[lo] + people[hi] <= limit) lo++;
-            hi--;
+        int i = 0, j = people.size()-1, w, cnt, ans = 0;
+        if (i == j) return 1;
+        while (i <= j) {
+            w = 0;
+            cnt = 0;
+            while (w + people[j] <= limit && cnt < 2 && i < j) {
+                w += people[j--];
+                cnt++;
+            }
+            while (w + people[i] <= limit && cnt < 2 && i <= j) {
+                w += people[i++];
+                cnt++;
+            }
             ans++;
         }
         return ans;
