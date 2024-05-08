6bae555407a36a6b538f486e4204aa5ce0d53791
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-05-08 11:54:08 +0900

    leetcode 451 - study

--- Leetcode/451_Sort_Characters_By_Frequency.cpp
+++ Leetcode/451_Sort_Characters_By_Frequency.cpp
@@ -3,15 +3,13 @@ public:
     string frequencySort(string s) {
         map<char, int> m;
         for (auto c: s) m[c]++;
-        vector<pair<char, int>> v(m.begin(), m.end());
-        sort(v.begin(), v.end(), [](pair<char, int> a, pair <char,int> b) -> bool {
-            if (a.second > b.second) return true;
-            else return false;
-        });
+        priority_queue<pair<int, char>> pq;
+        for (auto [c, i]: m) pq.push({i, c});
         s = "";
-        for (auto p: v) {
+        while (!pq.empty()) {
             int i = 0;
-            while (i++ < p.second) s += p.first;
+            while (i++ < pq.top().first) s += pq.top().second;
+            pq.pop();
         }
         return s;
     }
