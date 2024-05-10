d92ee46043b0edc252ca39dcec9648eddcaf46e4
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-05-10 14:20:25 +0900

    leetcode 852 - study

--- Leetcode/852_Peak_Index_in_a_Mountain_Array.cpp
+++ Leetcode/852_Peak_Index_in_a_Mountain_Array.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int lo = 1, hi = arr.size()-2, mid;
-        while (lo < hi) {
-            mid = (lo + hi) / 2;
-            if (arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]) return mid;
-            else if (arr[mid] < arr[mid+1]) lo = mid + 1;
-            else hi = mid - 1;
+        int n = arr.size(), lo = 0, hi = n-1;
+        while (lo <= hi) {
+            int mid = (lo+hi)/2;
+            if (mid == 0 || mid == n-1 || (arr[mid-1] < arr[mid] && arr[mid+1] < arr[mid])) return mid;
+            else if (arr[mid-1] > arr[mid]) hi = mid;
+            else lo = mid;
         }
-        return hi;
+        return arr[lo];
     }
 };
