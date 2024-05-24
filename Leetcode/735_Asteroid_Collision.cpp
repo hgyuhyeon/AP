c991626f4b3106c3cfa29fc32adeb8e6762e1921
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-05-24 13:07:12 +0900

    leetcode 735 - study

--- Leetcode/735_Asteroid_Collision.cpp
+++ Leetcode/735_Asteroid_Collision.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         stack<int> s;
-        for (auto a: asteroids) {
-            if (s.empty()) s.push(a);
-            else {
-                if ((a > 0 && s.top() > 0) || (a < 0 && s.top() < 0)) {
-                    s.push(a);
+        for (auto i: asteroids) {
+            int val = i;
+            while (!s.empty() && (s.top() > 0 && i < 0)) {
+                int a = s.top(), b = abs(i);
+                if (a >= b) {
+                    if (a == b) s.pop();
+                    val = 0;
+                    break;
                 }
-                else { // collide
-                    while (!s.empty() && s.top() > 0 && s.top() < abs(a)) s.pop();
-                    if (!s.empty() && (s.top() == abs(a))) s.pop();
-                    else if (!s.empty() && (s.top() > abs(a))) continue;
-                    else s.push(a);
-                }
+                s.pop();
             }
+            if (val) s.push(val);
         }
-
         vector<int> ans(s.size());
         for (int i = ans.size() - 1; i >= 0; i--) {
             ans[i] = s.top();
