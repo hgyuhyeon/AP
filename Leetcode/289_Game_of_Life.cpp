6caef93021577c374a09cc8ab7fc92a93b6032e9
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-06-13 13:19:03 +0900

    leetcode 289 - study

--- Leetcode/289_Game_of_Life.cpp
+++ Leetcode/289_Game_of_Life.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    int calc(vector<vector<int>>& board, int r, int c) {
-        int val = 0;
-        int i = r-1 < 0 ? 0 : r-1, j = c-1 < 0 ? 0 : c-1;
-        int n = r+1 >= board.size() ? r : r+1, m = c+1 >= board[0].size() ? c : c+1;
-        for (;i <= n; i++) {
-            for (int k = j; k <= m; k++) {
-                val += board[i][k];
+    int calc(vector<vector<int>>& board, int x, int y) {
+        int cnt = 0, n = board.size(), m = board[0].size();
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) continue;
+                int r = x + i, c = y + j;
+                if (r >= 0 && r < n && c >= 0 && c < m && board[r][c]) cnt++;
             }
         }
-        return val - board[r][c];
+        if (cnt == 3 || (board[x][y] && cnt == 2)) return 1;
+        else return 0;
     }
     void gameOfLife(vector<vector<int>>& board) {
         int n = board.size(), m = board[0].size();
         vector<vector<int>> tmp(n, vector<int>(m, 0));
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                int neighbor = calc(board, i, j);
-                if (neighbor > 3 || neighbor < 2) tmp[i][j] = 0;
-                else if (neighbor == 3) tmp[i][j] = 1;
-                else tmp[i][j] = board[i][j];
+                tmp[i][j] = calc(board, i, j);
             }
         }
-
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) board[i][j] = tmp[i][j];
+            for (int j = 0; j < m; j++) {
+                board[i][j] = tmp[i][j];
+            }
         }
     }
 };
