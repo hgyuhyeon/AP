92c4a0ab63224bafc31c6fb6003983a96ca323b1
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-06-14 09:49:11 +0900

    leetcode 945 - daily

---
+++ Leetcode/945_Minimum_Increment_to_Make_Array_Unique.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size(), ans = 0;
+        for (int i = 1; i < n; i++) {
+            if (nums[i-1] >= nums[i]) {
+                ans += (nums[i-1] + 1 - nums[i]);
+                nums[i] = nums[i-1] + 1;
+            }
+        }
+        return ans;
+    }
+};
