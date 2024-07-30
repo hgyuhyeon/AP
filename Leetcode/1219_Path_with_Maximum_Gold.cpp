c1ba5e37c20bceefc61ffd7972a53757ae3aaec4
GyuHyeon Hwang <57509844+hgyuhyeon@users.noreply.github.com>
2024-07-30 15:39:23 +0900

    leetcode 1219

--- Leetcode/1219_Path_with_Maximum_Gold.cpp
+++ Leetcode/1219_Path_with_Maximum_Gold.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& g, vector<vector<bool>>& v, int i, int j, int cur, int& ans) {
+    int n, m, ans = 0;
+    int dr[4] = {1,0,-1,0}, dc[4] = {0,1,0,-1};
+    void dfs(vector<vector<int>>& g, vector<vector<bool>>& v, int i, int j, int cur) {
         if (g[i][j] == 0) return;
         ans = max(ans, cur);
-        if (i > 0 && !v[i-1][j]) {
-            v[i-1][j] = true;
-            dfs(g, v, i-1, j, cur+g[i-1][j], ans);
-            v[i-1][j] = false;
+        for (int d = 0; d < 4; d++) {
+            int nr = i + dr[d], nc = j + dc[d];
+            if (nr >= 0 && nr < n && nc >= 0 && nc < m && !v[nr][nc]) {
+                v[nr][nc] = true;
+                dfs(g, v, nr, nc, cur+g[nr][nc]);
+                v[nr][nc] = false;
+            }
         }
-        if (j > 0 && !v[i][j-1]) {
-            v[i][j-1] = true;
-            dfs(g, v, i, j-1, cur+g[i][j-1], ans);
-            v[i][j-1] = false;
-        }
-        if (i < g.size()-1 && !v[i+1][j]) {
-            v[i+1][j] = true;
-            dfs(g, v, i+1, j, cur+g[i+1][j], ans);
-            v[i+1][j] = false;
-        }
-        if (j < g[0].size()-1 && !v[i][j+1]) {
-            v[i][j+1] = true;
-            dfs(g, v, i, j+1, cur+g[i][j+1], ans);
-            v[i][j+1] = false;
-        }
-
     }
     int getMaximumGold(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size(), ans = 0;
-        
+        n = grid.size();
+        m = grid[0].size();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] != 0) {
                     vector<vector<bool>> v(n, vector<bool>(m, false));
                     v[i][j] = true;
-                    dfs(grid, v, i, j, grid[i][j], ans);
+                    dfs(grid, v, i, j, grid[i][j]);
                 }
             }
         }
